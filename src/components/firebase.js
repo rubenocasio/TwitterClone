6f5749be629faa6312d2b16a412da1fc28c9c28c
Ruben Ocasio <rocasio@rubenocasio.com>
2024-01-18 11:48:13 -0500

    !

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -2,7 +2,7 @@ import firebase from 'firebase'
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-
+    //Secret ENV
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig)
